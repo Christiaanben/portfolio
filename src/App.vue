ff3647eb346e2c79389995c79d50a1258df46ccd
Bennie <bennie@comparisure.co.za>
2024-04-24 01:40:52 +0200

    test v3 deployment

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App v2"/>
+  <HelloWorld msg="Welcome to Your Vue.js App v3"/>
 </template>
 
 <script>
