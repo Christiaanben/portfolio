24a79f0a1024955df70c9025f68aa83d65b78c5f
Bennie <bennie@comparisure.co.za>
2024-04-29 03:05:03 +0200

    add contact card icons

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,33 @@
         avatar-url="profile-photo.jpg"
     />
     <div class="cards-container">
-      <info-card style="grid-column: 1 / -1">
-        <strong>Bennie van Eeden</strong>
-        <p>Full Stack Developer</p>
+      <info-card class="identity-card">
+        <div>
+          <b>Bennie van Eeden</b>
+          <div>Full Stack Developer</div>
+        </div>
+        <div>rather.chat</div>
       </info-card>
       <info-card style="grid-row: 2 / -1">Join Bennie's Meeting</info-card>
       <info-card>Hello! I'm in South Africa</info-card>
       <info-card style="grid-row: 3 / span 2">Their time: 21:24 SAST</info-card>
       <info-card>Schedule a meeting</info-card>
-      <info-card style="grid-row: 2 / span 4; grid-column: 3">Email LinkedIn Twitter CV</info-card>
+      <info-card class="contact-cards-container">
+        <info-card>
+          <img alt="Email" src="icons/email.png">
+        </info-card>
+        <a href="https://www.linkedin.com/in/bennie-van-eeden-22654a112/">
+          <info-card>
+            <img alt="LinkedIn" src="icons/linkedin.svg">
+          </info-card>
+        </a>
+        <info-card>
+          <img alt="Twitter" src="icons/twitter.svg">
+        </info-card>
+        <info-card>
+          <img alt="CV" src="icons/cv.png">
+        </info-card>
+      </info-card>
     </div>
   </div>
 </template>
@@ -63,6 +81,36 @@ export default {
   gap: 10px; /* Adjust gap as needed */
   max-width: 960px;
   width: 80%
+}
+
+.identity-card {
+  grid-column: 1 / -1;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.contact-cards-container {
+  grid-row: 2 / span 4;
+  grid-column: 3;
+
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  gap: 10px;
+  background-color: #e0e0e0 !important;
+}
+
+.info-card img {
+  max-width: 100%; /* ensures the image is not wider than the card */
+  max-height: 100%; /* ensures the image is not taller than the card */
+  object-fit: contain; /* scales the image to be as large as possible within the given dimension, maintaining aspect ratio */
+  display: block; /* removes extra space below the image */
+}
+
+a .info-card:hover {
+  background-color: #f0f0f0;
+  opacity: 0.9;
 }
 
 </style>
--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -18,7 +18,7 @@ export default defineComponent({
   padding: 12px;            /* Padding for internal spacing */
   border-radius: 12px;      /* Rounded corners */
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Subtle shadow for depth */
-  margin: 10px auto;        /* Centering in the page and providing some margin */
+  margin: 0 auto;        /* Centering in the page and providing some margin */
   box-sizing: border-box;   /* Includes padding in the width calculation */
   width: 100%;
   height: 100%;
